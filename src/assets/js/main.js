876906395cd98a4f39a13f516d7b705e8e824499
Ryan Dawes <ryan@dawes.cc>
2023-03-19 14:28:58 -0400

    updates

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,31 +1,31 @@
-      // get the modal element
-      var modal = document.getElementById("myModal");
+// get the modal element
+var modal = document.getElementById("myModal");
 
-      // get the close button for the modal
-      var closeBtn = document.getElementsByClassName("close")[0];
+// get the close button for the modal
+var closeBtn = document.getElementsByClassName("close")[0];
 
-      // when the user clicks on the close button, close the modal
-      closeBtn.onclick = function() {
-        modal.style.display = "none";
-      };
+// when the user clicks on the close button, close the modal
+closeBtn.onclick = function() {
+  modal.style.display = "none";
+};
 
-      function sendMemo() {
-        var memoContent = document.getElementById("memo-input").value;
-        if (memoContent === "") {
-          // if the text input field is empty, show the modal pop up
-          modal.style.display = "block";
-        } else {
-          // otherwise, send the memo data via HTTP POST request
-          modal.style.display = "none";
-          var memoData = {
-            "content": memoContent
-          };
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "https://memos.dawes.casa/api/memo?openId=05d3578b-8672-447c-8fff-dec4db3df6dc");
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.send(JSON.stringify(memoData));
-            document.getElementById("memo-input").value = ""; // clear the text box
-        }}
+function sendMemo() {
+  var memoContent = document.getElementById("memo-input").value;
+  if (memoContent === "") {
+    // if the text input field is empty, show the modal pop up
+    modal.style.display = "block";
+  } else {
+    // otherwise, send the memo data via HTTP POST request
+    modal.style.display = "none";
+    var memoData = {
+      "content": memoContent
+    };
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://memos.dawes.casa/api/memo?openId=05d3578b-8672-447c-8fff-dec4db3df6dc");
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.send(JSON.stringify(memoData));
+      document.getElementById("memo-input").value = ""; // clear the text box
+  }}
 
 const input = document.getElementById('memo-input');
 const button = document.getElementById('submit');
