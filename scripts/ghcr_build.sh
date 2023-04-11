4318b51632ae890fdfdc47071d763be4616aee51
Ryan Dawes <ryan@dawes.cc>
2023-04-10 23:12:57 -0400

    testing

---
+++ scripts/ghcr_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get the Git commit hash
+container="ghcr.io/dawescc/emems"
+vers="alpha"
+imagetag=$(git rev-parse --short HEAD)
+
+# Build the Docker image using the Dockerfile
+docker build -t $container:$vers-$imagetag -t $container:latest .
+
+docker push $container:latest
