de6f3d22e04a9cda46ad1fa94d296af551d0e9cb
Ryan Dawes <ryan@dawes.cc>
2023-04-16 18:22:41 -0400

    edited server.js to server static

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'));
 
 const db = new sqlite3.Database('database.db', (err) => {
     if (err) {
