45d04392e046e52cc1ea4bf806eeef4fa4ec84e7
Ryan Dawes <ryan@dawes.cc>
2023-04-16 19:16:09 -0400

    updated server.js and gitignore

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('dist'));
 
-const db = new sqlite3.Database('database.db', (err) => {
+const db = new sqlite3.Database('backend/database.db', (err) => {
     if (err) {
         console.error(err.message);
     } else {
